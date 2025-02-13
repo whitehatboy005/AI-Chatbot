e83c3c4be61ee1918ba302446b6ef3f4fe20bae4
Harishkumar <147156726+whitehatboy005@users.noreply.github.com>
2025-02-13 08:47:49 +0530

    Update Ai.py

--- Ai.py
+++ Ai.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv('config.env')
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.getenv("GEMINI_API")
 NAME = os.getenv("NAME")
 
 genai.configure(api_key=API_KEY)
